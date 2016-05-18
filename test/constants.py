a6a0774478925dab9951d3ec400c15dba120a918
Nidhi <nidhi@skimlinks.com>
2016-05-18 14:21:26 +0100

    done

---
+++ test/constants.py
@@ -0,0 +1,67 @@
+#General
+LOCAL_HUB_URL = "http://hub.skimlinks.com/"
+USERNAME = "nic@skimlinks.com"
+PASSWORD = "skim <3"
+
+#Common top menu
+SETUP_ID = "setup"
+TOOLBOX_ID = "optimize"
+MERCHANTS_ID = "merchants"
+PERFORMANCE_ID = "performance"
+SNAPSHOT_ID = "snapshot"
+HUB_LOGO_CLASS = "nav__logo--pub-hub"
+
+#Common side menu
+LOGOUT_ID = "logout"
+SUPPORT_LINK_ID = "support-link"
+UNIVERSITY_LINK_ID = "university-link"
+PAYMENTS_LINK_ID = "payments-link"
+ACCOUNT_LINK_ID = "account-link"
+CLOSE_SIDE_NAVIGATION_CLASS = "navigation__burger close-side-navigation"
+OPEN_SIDE_NAVIGATION_CLASS = "navigation__burger open-side-navigation"
+
+
+#Forgot password page
+FORGOT_EMAIL_FIELD_ID = "forgot_email"
+LOGINBUTTON_CLASS = "loginbutton"
+
+#Login Page
+LOGIN_PAGE_URL = "http://hub.skimlinks.com/login"
+LOGIN_FORGOTPASS_CLASS = "forgotpassword"
+LOGIN_SIGNUP_CLASS = "signup"
+LOGIN_CONTACT_BUTTON_CLASS = "contactbutton"
+LOGIN_LOGIN_BUTTON_ID = "btn-login"
+LOGIN_REMEMBER_CHECKBOX_ID = "remember"
+LOGIN_PASSWORD_FIELD_ID = "password"
+LOGIN_LOGIN_FIELD_ID = "username"
+
+#Snapshot Page
+REVENUE_BTN_ID = "revenue-btn"
+CLICKS_BTN_ID = "clicks-btn"
+TOPPAGES_BTN_ID = "toppages-btn"
+TOPSITES_BTN_ID = "topsites-btn"
+APPROVEDMERCHANTS_BTN_ID = "approvedmerchants-btn"
+TOPMERCHANTS_BTN_ID = "topmerchants-btn"
+TOPPRODUCTS_BTN_ID = "topproducts-btn"
+
+SNAPSHOT_APPROVED_MERCHANTS_ID = "snapshot-approved-merchants"
+SNAPSHOT_TOP_PAGES_ID = "snapshot-top-pages-col"
+SNAPSHOT_TOP_CHARTS_ID = "snapshot-top-charts-col"
+SNAPSHOT_REVENUE_GRAPH_ID = "snapshot-revenue-graph"
+
+#Setup Page
+SKIMWORDS_BUTTON_ID = "skimwords-button"
+SKIMLINKS_BUTTON_ID = "skimlinks-button"
+STP_DOMAIN_DROPDOWN_ID = 'settings-domain-picker'
+STP_INSTALL_DROPDOWN_ID = 'stp_install_dropdown'
+SETTINGS_NAV_ID = "settings-nav"
+INSTALL_NAV_ID = "install-nav"
+
+#Merchants page
+MERCHANTS_OVERVIEW_CLASS = "overview-featured"
+MERCHANTS_SEARCH_BOX_ID = "merchants-search-box"
+MERCH_COUNTRY_DROPDOWN_ID = 'merch_country_dropdown'
+MERCH_CATEGORY_DROPDOWN_ID = 'merch_category_dropdown'
+POPULAR_MERCHANTS_BUTTON_ID = "popular-merchants-button"
+PREFFERED_PARTNERS_ID = "preferred-partners-button"
+APPLY_BUTTON_CLASS = "merchants-filter-apply s-form-action-button"
